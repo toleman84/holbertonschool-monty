9eeac37f708e9e2d578db934a4c254b858009ab6
toleman84 <113698453+toleman84@users.noreply.github.com>
2023-05-08 14:55:36 -0300

    add files

---
+++ functions.c
@@ -0,0 +1,24 @@
+/**
+ * push - Descriotion
+ * stack: first member
+ * line_number: second member
+ * 
+ * Return: 
+ */
+
+// The opcode push pushes an element to the stack.
+// Use the atoi function  (convierte una cadena en valor numérico)
+
+int push(stack_t **stack, unsigned int line_number)
+{
+
+    if (!stack)
+    {
+        print('L<line_number>: usage: push integer\n', line_number)
+        exit(EXIT_FAILURE)
+    }
+    else
+    {
+
+    }
+}

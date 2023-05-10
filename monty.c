69e4085a1372a1a827ae5537a411a36031edcdf6
toleman84 <113698453+toleman84@users.noreply.github.com>
2023-05-10 10:21:29 -0300

    adds

--- monty.c
+++ monty.c
@@ -16,10 +16,14 @@
     // file open: FILE *fopen(const char *file_name, const char *mode_of_operation);
     FILE *file_name = fopen(argv[1], 'r+');
 
+    // malloc: void *malloc(size_t size);
+    line = (char**) malloc(sizeof(char)*1000);
+
     // getline: size_t getline (char **string, size_t *n, FILE *stream);
     while(getline(&line, &len, file_name))
     {
       // token
+      
       // string compare
     }
 
@@ -38,9 +42,30 @@
    {'function', comand}
  };
 
+/**
+ * string_compare - Short description
+ * @:
+ * Return void 
+ */
+
  void string_compare()
  {
-   
+  int index = 0;
+
+  while (array[index].opcode)
+  {
+   // int strcmp(const char *s1, const char *s2);
+   strcmp();
+  }
  }
 
- // string token
+/**
+ * token - Short description
+ * 
+ */
+
+int token()
+{
+  // string token: char *strtok(char *restrict str, const char *restrict delim);
+  strtok(line, '\t\r\a\n ;:');
+}
